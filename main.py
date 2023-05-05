27e81e981df14ca27a0e0f02cc329db469994a3d
beyondyouth <beyondyouth@qq.com>
2023-05-05 09:33:37 +0800

    重命名文件，新建爬虫文件

---
+++ main.py
@@ -0,0 +1,14 @@
+
+class DamaiSpider(object):
+    """
+    DamaiSpider 类
+    """
+    def __init__(self):
+        self.headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36 Edg/85.0.564.51"}
+    
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    spider = DamaiSpider()
+    spider.run()
